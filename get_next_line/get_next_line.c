1b22a55a81779f30346e76bf7df45ad02e14ca1e
mario-asenjo <mario.asenjo@outlook.es>
2025-09-26 12:09:40 +0200

    Added folders and brainfuck code

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,9 @@
+#include "get_next_line.h"
+
+int main(void)
+{
+	char	*line;
+	
+	line = get_next_line(4);
+
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
