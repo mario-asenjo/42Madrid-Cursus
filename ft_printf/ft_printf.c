e1b694e7df3bee8369d2e9f6a1783076641b271c
mario-asenjo <mario.asenjo@outlook.es>
2025-10-07 16:55:31 +0200

    Added ft_printf changes

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -12,60 +12,54 @@
 
 #include "ft_printf.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
+int	print_and_return_count(va_list args, char const *str)
 {
 	size_t	i;
+	int		count;
 
+	count = 0;
 	i = 0;
 	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == 'c')
+				count += ft_print_count_char(va_arg(args, int));
+			else if (str[i] == 's')
+				count += ft_print_count_str(va_arg(args, char *));
+			else if (str[i] == 'd')
+				count += ft_print_count_nbr(va_arg(args, int), "0123456789");
+		}
+		else
+			count += ft_print_count_char(str[i]);
 		i++;
-	write(1, str, i);
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb / 10 > 0)
-		ft_putnbr(nb / 10);
-	ft_putchar(nb % 10 + '0');
+	}
+	return (count);
 }
 
 int	ft_printf(char const *str, ...)
 {
 	va_list	ap;
-	char	*s;
-	int		d;
-	size_t	ind;
+	int		count;
 
-	ind = 0;
+	if (!str)
+		return (-1);
 	va_start(ap, str);
-	while (str[ind])
-	{
-		if (str[ind] == '%' && str[ind + 1] == 's')
-		{
-			s = va_arg(ap, char *);
-			ft_putstr(s);
-			ind += 2;
-		}
-		if (str[ind] == '%' && str[ind + 1] == 'd')
-		{
-			d = va_arg(ap, int);
-			ft_putnbr(d);
-			ind += 2;
-		}
-		write(1, &str[ind], 1);
-		ind++;
-	}
-	return (1);
+	count = print_and_return_count(ap, str);
+	va_end(ap);
+	return (count);
 }
 
 int	main(void)
 {
-	if (!ft_printf("%s %s %s %d %s", "hola soy ", "Mario", " tengo ", 12, " años y me gusta todo"))
+	int	count_mine = 0;
+	int count_printf = 0;
+	if (!ft_printf("%s %s!, tienes %d años?", "Hola", "Juan", 18))
 		return (write(2, "Error.\n", 7), 1);
+	count_printf = printf("Hola %s %d!", "Mario", -22);
+	count_mine = ft_printf("Hola %s %d!", "Mario", -22);
+	printf("count_printf: %d", count_printf);
+	printf("count_mine: %d", count_mine);
 	return (0);
 }
