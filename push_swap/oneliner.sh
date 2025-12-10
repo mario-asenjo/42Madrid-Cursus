b69bcadcb6281ce247618a2186cfe4df3de7d5a7
Mario Asenjo Pérez <56912864+mario-asenjo@users.noreply.github.com>
2025-12-10 17:17:31 +0100

    Replace zsh script with bash and update ranges

--- push_swap/oneliner.sh
+++ push_swap/oneliner.sh
@@ -2,3 +2,6 @@
 
 while true; do NBR=$(seq -1000 1000 | sort -R | head -500 | tr '\n' ' '); ./push_swap $NBR > salida | ./checker $NBR ; cat salida | wc -l; sleep 1; done
 
+#!/bin/bash
+
+while true; do NBR=$(seq -10000 10000 | sort -R | head -5000 | tr '\n' ' '); ./push_swap $NBR > salida ; cat salida | wc -l | tr '\n' ' '; < salida ./checker_linux $NBR; sleep 0.1; done
